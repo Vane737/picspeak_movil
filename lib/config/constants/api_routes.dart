4a9b07a1a96c7516038abff121da7f82746ace06
Christian Celso <christiancelsomamanisoliz0@gmail.com>
2024-01-13 23:13:43 -0400

    join main y mesa

--- lib/config/constants/api_routes.dart
+++ lib/config/constants/api_routes.dart
@@ -1,6 +1,6 @@
 // ------- STRINGS ------------------
-// const baseUrl = "https://app-picspeak-66m7tu3mma-uc.a.run.app/api/v1";
-const baseUrl = "http://192.168.242.118:3000/api/v1";
+const baseUrl = "https://app-picspeak-66m7tu3mma-uc.a.run.app/api/v1";
+// const baseUrl = "http://192.168.242.118:3000/api/v1";
 const loginUrl = '$baseUrl/auth/login';
 const registerUrl = '$baseUrl/auth/register';
 const logoutUrl = '${baseUrl}logout';
